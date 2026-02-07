804f54709ada0d3d24e5772190d794958c9d5fe1
mrzaakir <mrzaakiraj@gmail.com>
2026-02-07 17:44:04 +0300

    Submited-Exercise-1

---
+++ Exercise-1/Arithmeticts.js
@@ -0,0 +1,44 @@
+// Exercise For Arithmetic Operations in JavaScript
+
+// Addition
+let x = 7;
+let y = 3;
+let sum = x + y;
+console.log("Addition: " + sum); 
+
+console.log("   ");
+
+// Subtraction
+let difference = x - y;
+console.log("Subtraction: " + difference); 
+
+console.log("   ");
+
+// Multiplication
+let product = x * y;
+
+console.log("Multiplication:" + product) 
+
+console.log("   ");
+
+// Division
+let quotient = x / y;
+console.log("Division: " + quotient); 
+
+console.log("   ");
+
+// Modulus
+let remainder = x % y;
+console.log("Modulus: " + remainder); 
+
+console.log("   ");
+
+// Pre-Increment
+console.log("Pre-Increment: " + ++x);
+
+console.log("   ");
+
+// Pre-Decrement
+console.log("Pre-Decrement: " + --x); 
+
+
