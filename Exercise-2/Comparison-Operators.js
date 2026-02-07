985f2b04675d2cc66c84e34817e6693eb6761849
mrzaakir <mrzaakiraj@gmail.com>
2026-02-07 18:16:19 +0300

    submited-Exercise-2

---
+++ Exercise-2/Comparison-Operators.js
@@ -0,0 +1,34 @@
+// Comparison Operators in JavaScript
+
+
+let a = 15;
+let b = "15";
+
+// Equality (==)
+console.log("Equality (==): " + (a == b)); 
+
+// Not Equal (!=)
+console.log("Not Equal (!=): " + (a != b)); 
+
+// Strict Equality (===)    
+console.log("Strict Equality (===): " + (a === b)); 
+
+// Strict Not Equal (!==)
+console.log("Strict Not Equal (!==): " + (a !== b)); 
+
+// Greater Than (>)
+console.log(`Greater Than (>): ${a > b}`); 
+
+// Less Than (<)
+console.log(`Less Than (<): ${a < b}`); 
+
+// Greater Than Or Equal To (>=)
+console.log(`Greater Than Or Equal To (>=): ${a >= b}`) 
+
+// Less Than Or Equal To (<=)
+console.log(`Less Than Or Equal To (<=): ${a <= b}`) 
+
+
+
+
+
