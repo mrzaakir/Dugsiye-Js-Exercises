4f7b56a49b58abb041a26ddc491c0f35be5c2412
mrzaakir <mrzaakiraj@gmail.com>
2026-02-07 19:09:45 +0300

    submited-Exercise-3

---
+++ Exercise-3/Logical-Operators.js
@@ -0,0 +1,24 @@
+// Logical Operators in JavaScript
+
+let isSunny = false;
+let isWeekend = true;
+
+
+// Logical AND (&&)
+console.log(`Logical AND (&&): ${isSunny && isWeekend}`); 
+
+console.log('   ');
+
+// Logical OR (||)
+console.log(`Logical OR (||): ${isSunny || isWeekend}`); 
+
+console.log('   ');
+
+// Logical NOT (!)
+console.log(`Logical NOT (!): ${!isSunny}`); 
+
+console.log('   ');
+
+console.log(`Logical NOT (!): ${!isWeekend}`); 
+
+
