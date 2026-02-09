fdb6f9a6dc696b1ca87a9b922be7aafe6e2bfbca
mrzaakir <mrzaakiraj@gmail.com>
2026-02-09 20:53:01 +0300

    ArrowFunction

---
+++ Exercise-5/functionArrow.js
@@ -0,0 +1,20 @@
+// function Arrow 
+
+const arrowFunction =(b,z)=>{
+
+return(b+z)
+
+}
+
+console.log(arrowFunction(5,6));
+console.log(arrowFunction(5,50));
+console.log(arrowFunction(88,16));
+console.log(arrowFunction(100,900));
+console.log(arrowFunction(99,1));
+
+
+console.log(' ');
+
+let result = arrowFunction(100,900);
+
+console.log(`The result of the arrow function when its divided by 2 is: ${result/2}`);
