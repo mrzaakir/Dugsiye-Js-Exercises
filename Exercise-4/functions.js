bf830f474493e77e23c25633e13534056378af1b
mrzaakir <mrzaakiraj@gmail.com>
2026-02-09 20:14:44 +0300

    function Decleration/expression

---
+++ Exercise-4/functions.js
@@ -0,0 +1,27 @@
+// function Decleration and function Expression
+
+console.log('Function Decleration');
+
+function add (b,z){
+
+    console.log(b+z);
+}
+
+add(5,6);
+add(5,50);
+add(88,16);
+add(100,900);
+add(99,1);
+
+
+console.log('Function Expression');
+
+const adds = function (b,z){
+    console.log(b+z);
+}
+
+adds(5,6);
+adds(5,50);
+adds(88,16);
+adds(100,900);
+adds(99,1);
