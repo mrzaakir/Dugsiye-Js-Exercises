a3a06e04bf13fb710470e64783235590c6f0dfe6
mrzaakir <mrzaakiraj@gmail.com>
2026-02-10 10:57:24 +0300

    Exercise-8 Submited

---
+++ Exercise-8/Object.js
@@ -0,0 +1,19 @@
+// Objects in JavaScript
+
+// Car object with properties like make, model, year...!
+
+
+let car = {
+    make: 'China',
+    model: 'Toyota',
+    year: 2020,
+};
+
+// Accessing the properties of the car object
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+// Accessing the all properties of the car object
+console.log(car);
+
