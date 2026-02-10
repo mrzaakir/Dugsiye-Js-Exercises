257b985922fdb2c8ac29e6cbc701851f05c70837
mrzaakir <mrzaakiraj@gmail.com>
2026-02-10 11:15:02 +0300

    Exercise-9 Submited

---
+++ Exercise-9/ObjectMethod.js
@@ -0,0 +1,21 @@
+//  Object Methods in javaScript Exercise
+
+// Objects in JavaScript
+
+// Car object with properties like make, model, year...!
+
+
+let car = {
+    make: 'China',
+    model: 'Toyota',
+    year: 2020,
+    start: function (){
+        console.log('The car has started');
+    }
+};
+
+console.log(car);
+
+
+console.log(car.start());
+
