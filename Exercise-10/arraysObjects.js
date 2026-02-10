b231613a8f059fcf5d661bc4e1ad1bf010850ae1
mrzaakir <mrzaakiraj@gmail.com>
2026-02-10 11:30:42 +0300

    Exercise-10 ArraysInsideObjects

---
+++ Exercise-10/arraysObjects.js
@@ -0,0 +1,22 @@
+// Arrays inside Objects in javaScript Exercise 
+
+//  Library object with properties like title, author, year...!
+
+let libary = [
+    {
+        title: "48 Laws of Power",
+        author: "Robert Greene",
+        year: 1998
+    },
+    {
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        year: 1925
+    }
+]
+
+// the original array of objects
+console.log(libary);
+
+console.log(libary[0].title)
+console.log(libary[1].author)
