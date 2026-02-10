08f85e7a8bc09dc37d4d6be9111b4964283b62c3
mrzaakir <mrzaakiraj@gmail.com>
2026-02-10 10:39:59 +0300

    Exercise-7 Submited

---
+++ Exercise-7/ArraysMethods.js
@@ -0,0 +1,23 @@
+// Arrays Methods in javaScript
+
+
+// Using Methods of array like push, shift, then Loging The Array
+
+let colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+console.log(colors);
+
+// before using methods
+console.log('Before using methods:');
+console.log(colors);
+
+
+// Using push method to add a new color to the end of the array
+colors.push('gray');
+console.log('After using push method:');
+console.log(colors);
+
+// Using shift method to remove the first color from the array
+colors.shift();
+console.log('After using shift method:');
+console.log(colors);
+
