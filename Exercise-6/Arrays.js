ab6d295bdaa740ebd3fd4cbef2827d0017c5e6ac
mrzaakir <mrzaakiraj@gmail.com>
2026-02-10 10:20:40 +0300

    submiting Exercise-6 for arrays

---
+++ Exercise-6/Arrays.js
@@ -0,0 +1,15 @@
+// Arrays in javaScript
+
+
+// 1. Creating an array for colors
+
+let colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+console.log(colors);
+
+
+// calling by an individual index
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
