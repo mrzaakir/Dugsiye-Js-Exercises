7b8780aea5769efab3bef740ddd363ed0c0d346f
mrzaakir <mrzaakiraj@gmail.com>
2026-02-11 12:37:39 +0300

    Do While Loop Exercise-14

---
+++ Exercise-14/DoWhileLoop.js
@@ -0,0 +1,10 @@
+//  DoWhile Loop in JavaScript Exercise
+
+let password 
+
+do{
+    password = prompt("Enter your password");
+}while (password < 5) 
+
+alert("Welcome sir to the system");
+console.log("The valid password is The Number of Greater Than 5: " + password);
