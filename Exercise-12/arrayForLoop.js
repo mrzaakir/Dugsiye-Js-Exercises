a2bcd6323b076bd1bdb77bf00930556a9ae25b09
mrzaakir <mrzaakiraj@gmail.com>
2026-02-11 11:20:49 +0300

    Exercise-12 submited

---
+++ Exercise-12/arrayForLoop.js
@@ -0,0 +1,10 @@
+// Arrays and for Loop in JavaScript Exercise
+
+let fruits = ["Apple", "Banana", "Cherry", "Mango", "Lemon"];
+
+for (let Z =0; Z< fruits.length; Z++){
+
+    console.log(fruits[Z])
+}
+
+console.log(fruits.length);
