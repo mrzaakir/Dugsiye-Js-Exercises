1cc0a4216879013f15b0947ccc44665fda44ffb4
mrzaakir <mrzaakiraj@gmail.com>
2026-02-14 20:13:23 +0300

    else-if-Exercise-17

---
+++ Exercise-17/else-if.js
@@ -0,0 +1,16 @@
+//  Else if statements in js Exercise
+
+const temp = -9;
+
+if(temp <= 0){
+    console.log('It is Very Cold outside');
+}else if(temp > 0 && temp < 15){
+    console.log('It is cold outside');
+}else if(temp >= 15 && temp < 25){
+    console.log('It is warm outside');
+}else{
+    console.log('It is hot outside');
+}   
+
+
+
