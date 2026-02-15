2b764fde35665da097d631b6534cb57f0d25efb2
mrzaakir <mrzaakiraj@gmail.com>
2026-02-15 20:55:03 +0300

    Exercise-20 Map-Method

---
+++ Exercise-20/mapMethod.js
@@ -0,0 +1,11 @@
+// Map Method in js Exercise......
+
+const fruits = ['apple', 'banana', 'orange','mango', 'pineapple'];
+
+const fruitslength= fruits.map((fruits)=>{
+    return fruits.length;
+});
+
+
+console.log(`${fruitslength}`);
+
