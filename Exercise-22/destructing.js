af449c0874615fa32b63c9de9cb1a93a18d2a4b2
mrzaakir <mrzaakiraj@gmail.com>
2026-02-19 16:17:30 +0300

    Destructing Exercise-22

---
+++ Exercise-22/destructing.js
@@ -0,0 +1,9 @@
+// Destructing in js Exercise......
+
+const midabo = ['gudud', 'bulug', 'jaalo', 'madow','cadan'];
+
+const [kowad, labad, sedexad, afarad, shanaad] = midabo;
+
+console.log(`Midabka kowad waa ${kowad}\nHalka midabka sedexad uu yahay ${sedexad}`)
+
+
