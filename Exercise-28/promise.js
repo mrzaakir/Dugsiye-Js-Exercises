80c5e167ed03263e588cd938ced585ddfbc7fad6
mrzaakir <mrzaakiraj@gmail.com>
2026-02-23 15:42:18 +0300

    Async/Await Exercise-28

---
+++ Exercise-28/promise.js
@@ -0,0 +1,45 @@
+//  Promises in js Using Async/Await Exercise........!
+
+
+
+const fetchData = () => {
+
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            const successMessage = false;
+
+            if(successMessage){
+                resolve([{id:101,name:"Zacky",age:23, country:'Somalia', city:'Muqdisho'},{id:102,name:"Bella",age:20, country:'kenya', town:'Bangali'}]);
+            }else{
+                reject('Oh MR/Ms fetching data not available now, try again later!');
+            }
+
+        },2000);
+    });
+}
+
+
+// fetchData().then((info)=>{
+//     console.log(`Success Message:`,info);
+// }).catch((qalad)=>{
+//     console.log(`Error:${qalad}`);
+// });
+
+async function getData(){
+    try{
+        const info = await fetchData();
+        console.log(`Success Message:`,info);
+
+    }catch(qalad){
+        console.log(`Error:`,qalad);
+    };
+}
+
+
+getData();
+
+
+
+
+
