a3951a534e24e40b6ad4528a21bb6199350b3b43
mrzaakir <mrzaakiraj@gmail.com>
2026-03-11 17:52:15 +0300

    DOM-Exercise-32

---
+++ Exercise-32/Dom.js
@@ -0,0 +1,11 @@
+const head = document.querySelector(".head");
+
+head.style.color = "blue";
+head.style.fontSize = "30px";
+
+console.log(head);
+
+const allParagraphs = document.querySelectorAll("p");
+
+
+console.log(allParagraphs);
