592b240627bf09841ead23d593bedd8f0bc7480f
mrzaakir <mrzaakiraj@gmail.com>
2026-03-31 20:42:33 +0300

    Changed Content Exercise-33

---
+++ Exercise-33/changinContent.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('.header');
+
+const text = document.querySelector('#text');
+
+
+
+function changeContact() {
+    header.textContent = 'Contact Information Changed! Welcome to DOM!';
+    
+}
+
+
+function changeElement() {
+    text.innerHTML = `Element Changed Welcome! <strong> my first interactive website...!</strong>`;
+    
+}
+
